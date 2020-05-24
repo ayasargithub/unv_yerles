945973735a4b0de00ec0439045b7ff176226c98f
ayasar_git <ayse.yasar.iu@gmail.com>
2020-05-24 09:54:18 +0300

    unv_yerles 2. düzenleme

--- giris_app/admin.py
+++ giris_app/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import universite
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+
+class UnvResource(resources.ModelResource):
+
+    class Meta:
+        model = universite
+
+
+class UnvAdmin(ImportExportModelAdmin):
+    resource_class = UnvResource
+
+      
+
 # Register your models here.
 
-admin.site.register(universite)
+# admin.site.register(universite)
+admin.site.register(universite, UnvAdmin)  
--- unv_yerles/settings.py
+++ unv_yerles/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
