537fc68d644a205f09ae0149b1dfd94ea4321c10
ayasar_git <ayse.yasar.iu@gmail.com>
2020-05-25 21:34:41 +0300

    ilk commit

--- unv_yerles/wsgi.py
+++ unv_yerles/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import import_export
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unv_yerles.settings')
