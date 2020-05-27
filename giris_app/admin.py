4cfd91570954c36b03d38df3dbf8d0ecc458101b
ayasar_git <ayse.yasar.iu@gmail.com>
2020-05-27 21:28:05 +0300

    21

--- giris_app/admin.py
+++ giris_app/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from .models import universite
+from import_export.admin import ImportExportModelAdmin
       
-
+class UnvAdmin(ImportExportModelAdmin):
+    resource_class = universite
 # Register your models here.
 
 # admin.site.register(universite)
-admin.site.register(universite)  
+admin.site.register(universite, UnvAdmin)  
+
+
